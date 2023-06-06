9382e1e4b8ec70fdd906b0e8a1bea6062ed4f78c
Bryan Kleinberg <bmklei8p@Bryans-MacBook-Pro.local>
2023-06-06 15:40:01 -0400

    updated skills to have proper images instead of icons that show better, updated ui for skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,37 +1,18 @@
 import React from "react";
-import {
-  FaJava,
-  FaPython,
-  FaReact,
-  FaDocker,
-} from "react-icons/fa";
-import {
-  SiTypescript,
-  SiJavascript,
-  // SiFastapi,
-  SiPostgresql,
-  SiMongodb,
-} from "react-icons/si"
-import { 
-  DiDjango,
-} from "react-icons/di"
-// import {
-//   BiLogoSpringBoot,
-// } from "react-icons/bi"
-const skillsList = [
-  { id: 1, imageSrc: <FaJava />, text: 'Java' },
-  { id: 2, imageSrc: <FaPython />, text: 'Python' },
-  { id: 3, imageSrc: <SiTypescript />, text: 'TypeScript' },
-  { id: 4, imageSrc: <FaReact />, text: 'React' },
-  { id: 5, imageSrc: <SiJavascript />, text: 'Javascript'},
-  { id: 6, imageSrc: <DiDjango />, text: 'Django' },
-  // { id: 7, imageSrc: <SiFastapi />, text: 'FastApi' },
-  { id: 8, imageSrc: <SiPostgresql />, text: 'Postgres' },
-  { id: 9, imageSrc: <SiMongodb />, text: 'MongoDB' },
-  { id: 10, imageSrc: <FaDocker />, text: 'Docker' },
-  // { id: 11, imageSrc: <BiLogoSpringBoot />, text: 'Spring Boot' },
-]
 
+const skillsList = [
+  { id: 1, imageSrc: "img/skills/java_icon.png", text: 'Java' },
+  { id: 2, imageSrc: "img/skills/python_icon.png", text: 'Python' },
+  { id: 3, imageSrc: "img/skills/typescript_icon.png", text: 'TypeScript' },
+  { id: 4, imageSrc: "img/skills/react_icon.png", text: 'React' },
+  { id: 5, imageSrc: "img/skills/javascript_icon.png", text: 'Javascript'},
+  { id: 6, imageSrc: "img/skills/django_icon.png", text: 'Django' },
+  { id: 7, imageSrc: "img/skills/fastapi_icon.png", text: 'FastApi' },
+  { id: 8, imageSrc: "img/skills/postgres_icon.png", text: 'Postgres' },
+  { id: 9, imageSrc: "img/skills/mongodb_icon.png", text: 'MongoDB' },
+  { id: 10, imageSrc: "img/skills/docker_icon.png", text: 'Docker' },
+  { id: 11, imageSrc: "img/skills/springboot_icon.png", text: 'Spring Boot' },
+]
 const Skills = () => {
   return (
     <>
@@ -54,7 +35,7 @@ const Skills = () => {
             <li>
               <span className="dark-bg">2008-2012</span>
               <h6>Bachelors of Chemistry</h6>
-              <p>North Carolina State University . </p>
+              <p>North Carolina State University </p>
             </li>
           </ul>
         </div>
@@ -70,7 +51,7 @@ const Skills = () => {
                 <div className="skills-container">
                   <div className="skills-item">
                     <div className="skills-item-circle">
-                      {item.imageSrc}
+                    <img src={item.imageSrc} alt="icon" height={"64px"} width={"64px"}/>
                     </div>
                     <span className="color-theme">{item.text}</span>
                   </div>
